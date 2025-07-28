e21c812d1d11ccee54f87dd592628e14fc81f74e
v3ronez <v3ronez.dev@gmail.com>
2025-07-28 10:10:53 -0300

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {}!", name)
+}
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
