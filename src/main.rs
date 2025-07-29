5ac84f9305af239eb4cc409164dc314a13c60416
v3ronez <v3ronez.dev@gmail.com>
2025-07-28 21:36:40 -0300

    the firts handlers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("world");
     format!("Hello {}!", name)
 }
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
+            // .route("/{name}", Route::new().guard(guard::Get()))
             .route("/{name}", web::get().to(greet))
+            .route("/health", web::get().to(health_check))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
