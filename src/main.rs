58204cd30ab04f29aec825e190840929fbab2605
v3ronez <v3ronez.dev@gmail.com>
2025-08-02 15:10:49 -0300

    add simple terminal log on actix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use sqlx::Connection;
+use env_logger::Env;
 use std::net::TcpListener;
 use zero2prod::{configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let settings = configuration::get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", settings.application_port);
     let listener = TcpListener::bind(&address)?;
