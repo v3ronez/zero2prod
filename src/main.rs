f745f54d8d040e3443bbec3d1053071f7a1135b2
v3ronez <v3ronez.dev@gmail.com>
2025-08-04 20:26:36 -0300

    telemetry mod

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
-use env_logger::Env;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
-use zero2prod::{configuration, startup::run};
+use zero2prod::{configuration, startup::run, telemetry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(formatting_layer)
-        .with(JsonStorageLayer);
-    let _ = set_global_default(subscriber);
+    //tracing application
+    let subscriber = telemetry::get_subscriber(String::from("zero2prod"), String::from("info"));
+    telemetry::init_subscriber(subscriber);
 
     let settings = configuration::get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", settings.application_port);
