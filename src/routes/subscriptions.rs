f322cbb61deb279831c488ba304b3647bccddb23
v3ronez <v3ronez.dev@gmail.com>
2025-08-03 22:40:06 -0300

    add uuid v4, tracing, tracing-subscriber

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{HttpResponse, web};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -14,6 +15,15 @@ pub async fn subscribe(
     form: web::Form<Subscription>,
     connection: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_nam = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber in the database");
     let row = sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -25,11 +35,17 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(connection.get_ref())
+    .instrument(query_span)
     .await;
+
     match row {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(err) => {
-            eprintln!("{}", err.to_string());
+            tracing::error!(
+                "Request_ID {} - Failed to execute query: {:?}",
+                request_id,
+                err
+            );
             HttpResponse::BadRequest().finish()
         }
     }
