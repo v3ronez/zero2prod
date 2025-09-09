9ce176bd60cee58cff2d3cac4faadc26fb974e86
v3ronez <v3ronez.dev@gmail.com>
2025-09-09 19:54:10 -0300

    using try_from to validate the new subscriber

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -7,9 +7,19 @@ use uuid::Uuid;
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(Deserialize, Debug)]
-pub struct Subscription {
+pub struct FormData {
     name: String,
     email: String,
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(form: FormData) -> Result<NewSubscriber, Self::Error> {
+        let name = SubscriberName::parse(form.name)?;
+        let email = SubscriberEmail::parse(form.email)?;
+        Ok(NewSubscriber { name, email })
+    }
 }
 
 #[tracing::instrument(
@@ -20,19 +30,15 @@ pub struct Subscription {
         subscriber_email = %form.email
     )
 )]
-pub async fn subscribe(form: web::Form<Subscription>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
-        Err(_e) => return HttpResponse::BadRequest().finish(),
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
+        Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_e) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriver = NewSubscriber { name, email };
-    match insert_subscriber(&pool, &new_subscriver).await {
+
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
-        Err(_err) => HttpResponse::BadRequest().finish(),
+        Err(_err) => HttpResponse::InternalServerError().finish(),
     }
 }
 
