83c139330842292e5857a9d61c823d0a4631a218
v3ronez <v3ronez.dev@gmail.com>
2025-09-10 23:19:51 -0300

    create email client

---
+++ src/email_client.rs
@@ -0,0 +1,28 @@
+use reqwest::Client;
+
+use crate::domain::SubscriberEmail;
+
+struct EmailClient {
+    sender: SubscriberEmail,
+    http_client: Client,
+    base_url: String,
+}
+
+impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            http_client: Client::new(),
+            sender,
+            base_url,
+        }
+    }
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), String> {
+        todo!()
+    }
+}
