1352829d5955342b4070f4caf0030daa66d49627
v3ronez <v3ronez.dev@gmail.com>
2025-09-14 16:33:35 -0300

    [master] wip

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,7 +1,15 @@
 use reqwest::Client;
+use serde::Serialize;
 
 use crate::domain::SubscriberEmail;
-
+#[derive(Serialize)]
+struct SendEmailRequest {
+    from: String,
+    to: String,
+    subject: String,
+    html_content: String,
+    text_content: String,
+}
 pub struct EmailClient {
     pub sender: SubscriberEmail,
     pub http_client: Client,
@@ -23,6 +31,49 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
-        todo!()
+        let url = format!("{}/api/send/2317403", self.base_url);
+        let request_body = SendEmailRequest {
+            from: self.sender.as_ref().to_owned(),
+            to: recipient.as_ref().to_owned(),
+            subject: subject.to_owned(),
+            html_content: html_content.to_owned(),
+            text_content: text_content.to_owned(),
+        };
+        let builder = self.http_client.post(url).json(&request_body);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fake::{
+        Fake,
+        faker::{
+            internet::en::SafeEmail,
+            lorem::en::{Paragraph, Sentence},
+        },
+    };
+    use wiremock::{Mock, MockServer, ResponseTemplate, matchers::any};
+
+    use crate::{domain::SubscriberEmail, email_client::EmailClient};
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
     }
 }
