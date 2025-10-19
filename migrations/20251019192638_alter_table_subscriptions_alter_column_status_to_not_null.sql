91d14ecfc023684ab08574afded693c323b00b9b
v3ronez <v3ronez.dev@gmail.com>
2025-10-19 17:16:24 -0300

    crate table subscription_tokens

--- migrations/20251019192638_alter_table_subscriptions_alter_column_status_to_not_null.sql
+++ migrations/20251019192638_alter_table_subscriptions_alter_column_status_to_not_null.sql
@@ -2,5 +2,5 @@ BEGIN;
    UPDATE subscriptions
    SET status = 'confirmed' WHERE status IS NULL;
 
-ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
+ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULl;
 COMMIT;
