5a2837179cae39134c654b6b1af2d9bd9461163c
v3ronez <v3ronez.dev@gmail.com>
2026-02-19 21:43:16 -0300

    first route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+
+async fn great(request: HttpRequest) -> impl Responder {
+    let name = request.match_info().get("name").unwrap_or("ze");
+    format!("Hello {name}")
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/v1")
+                .route("/", web::get().to(great))
+                .route("/{name}", web::get().to(great)),
+        )
+    })
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await
 }
